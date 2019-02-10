9c87befcae58c7234c7f797186e2e7ac16860eff
Abhie <abhie.lp@gmail.com>
2019-02-10 22:14:37 +0530

    Added Profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+from . import models
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = "id", "user", "photo",
+    list_filter = "date_of_birth",
+    list_display_links = "id", "user",
+    search_fields = "user",
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+def save_dp(instance, filename):
+    username = instance.user.username
+    extension = filename.rsplit(".", 1)[1]
+    filename = username + "." + extension
+    return "/".join([username[0], filename])
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+    photo = models.ImageField(upload_to=save_dp, blank=True)
+
+    def __str__(self):
+        return self.user.username
