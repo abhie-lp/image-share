7ed78c368f60e45ec15ef319c5e1c71261fb449c
Abhie <abhie.lp@gmail.com>
2019-02-12 14:35:31 +0530

    Added view to like image using jQuery and AJAX

--- images/urls.py
+++ images/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("create/", views.image_create, name="image_create"),
+    path("like/", views.image_like, name="image_like"),
     path("detail/<int:id>/<slug:slug>/", views.image_detail, name="image_detail"),
 ]
--- images/views.py
+++ images/views.py
@@ -1,7 +1,9 @@
 from . import forms, models
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http.response import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 
 @login_required
@@ -31,3 +33,24 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(models.Image, id=id, slug=slug)
     return render(request, 'images/detail.html', {"section": "images", "image": image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if image_id and action:
+        try:
+            image = models.Image.objects.get(id=image_id)
+
+            if action == "like":
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            
+            return JsonResponse({"status": "ok"})
+        except:
+            pass
+    
+    return JsonResponse({"status": "ko"})
