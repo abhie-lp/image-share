5333a4fdac1a9e77ed470b60fdc732fbd3130992
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-20 14:23:02 +0530

    Profile model

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegistrationForm
+from .models import Profile
 
 
 def register(request):
@@ -11,6 +12,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(
                 request, "account/register_done.html", {"new_user": new_user}
             )
