a374051485c3dcc4e109f53417a978d4e887af55
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-23 17:13:28 +0530

    Show all images of user

--- images/urls.py
+++ images/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("detail/<int:pk>/<slug:slug>/", views.image_detail, name="detail"),
     path("like/", views.image_like, name="like"),
     path("create/", views.image_create, name="create"),
+    path("", views.image_list, name="list"),
 ]
--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
@@ -8,6 +9,26 @@ from common.decorators import ajax_required
 
 from .forms import ImageCreateForm
 from .models import Image
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 10)
+    page = request.GET.get("page")
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse("")
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, "image/list_ajax.html",
+                      {"section": "images", "images": images})
+    return render(request, "image/list.html",
+                  {"section": "images", "images": images})
 
 
 @ajax_required
