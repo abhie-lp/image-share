4faa5a775acfed8d8ca0758da40d43934ae73eaa
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-23 15:40:02 +0530

    Bookmark images from other websites

--- image_share/urls.py
+++ image_share/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("social-auth/", include("social_django.urls", namespace="social")),
+    path("images/", include("images.urls", namespace="images")),
     path('admin/', admin.site.urls),
     path("account/", include("account.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
