69ee6824c5c71b8a24931f7ae8edfad99fc2c1f2
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-23 23:23:39 +0530

    Intermediary Contact model for follow user

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = "user_from", "user_to", "created"
 
 
 @admin.register(Profile)
