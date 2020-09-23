3e8ff838c6a437373c7c84b631485c61c574a92b
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-23 16:23:14 +0530

    Like and Unlike image

--- images/urls.py
+++ images/urls.py
@@ -5,5 +5,6 @@ app_name = "images"
 
 urlpatterns = [
     path("detail/<int:pk>/<slug:slug>/", views.image_detail, name="detail"),
+    path("like/", views.image_like, name="like"),
     path("create/", views.image_create, name="create"),
 ]
